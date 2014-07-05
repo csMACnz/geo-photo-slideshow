587c40de686611a493d419dce0aaa881d71e2033
Mark Clearwater <csmacnz@csmac.co.nz>
2014-07-05 21:49:20 +1200

    Initial Checkin

---
+++ src/GeoPhotoSlideshow.Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using GeoPhotoSlideshow.Core;
+
+namespace GeoPhotoSlideshow.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Reading Data");
+
+            var dataservice = new DataService(new StaticDataLoader());
+
+            var dataLoadObservable = dataservice.GetDataStream(TimeSpan.FromSeconds(2));
+
+            ConsoleKey key = ConsoleKey.Home;
+            while (key != ConsoleKey.Escape)
+            {
+                CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+                dataLoadObservable.Subscribe(PrintPhoto, HandleError, HandleCompleted, tokenSource.Token);
+                
+                key = System.Console.ReadKey().Key;
+                
+                tokenSource.Cancel();
+            }
+            
+            System.Console.WriteLine("Press any key");
+
+            System.Console.ReadKey();
+        }
+
+        private static void HandleCompleted()
+        {
+            System.Console.WriteLine("Press Esc to exit");
+        }
+
+        private static void HandleError(Exception obj)
+        {
+            System.Console.WriteLine("Cancelled");
+        }
+
+        private static void PrintPhoto(Photo photo)
+        {
+            System.Console.WriteLine("Photo {0}({1}) at {2}", photo.Title, photo.Source, photo.Location);
+        }
+    }
+}
---
+++ src/GeoPhotoSlideshow.Core/DataService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace GeoPhotoSlideshow.Core
+{
+    public class DataService
+    {
+        private readonly IDataLoader _dataLoader;
+
+        public DataService(IDataLoader dataLoader)
+        {
+            _dataLoader = dataLoader;
+        }
+
+        public IObservable<Photo> GetDataStream(TimeSpan streamInterval)
+        {
+
+            return
+                Observable.FromAsync(_dataLoader.LoadPhotos)
+                    .SelectMany(s => s)
+                    .Zip(Observable.Interval(streamInterval), (p, t) => p);
+        }
+    }
+
+    public class Photo
+    {
+        public string Source { get; set; }
+        public Location Location { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class Location
+    {
+        private readonly int _xOffset;
+        private readonly int _yOffset;
+
+        public Location(int xOffset, int yOffset)
+        {
+            _xOffset = xOffset;
+            _yOffset = yOffset;
+        }
+
+        public int XOffset
+        {
+            get { return _xOffset; }
+        }
+
+        public int YOffset
+        {
+            get { return _yOffset; }
+        }
+    }
+
+    public interface IDataLoader
+    {
+        Task<Photo[]> LoadPhotos();
+    }
+
+    public class StaticDataLoader:IDataLoader
+    {
+        public Task<Photo[]> LoadPhotos()
+        {
+            return Task.Factory.StartNew(
+                () => new[]
+                {
+                    new Photo
+                    {
+                        Title="First Image",
+                        Source="http://placekitten.com/200/300",
+                        Location = new Location(1,1)
+                    },
+                    new Photo
+                    {
+                        Title="Second Image",
+                        Source="http://www.placecage.com/200/300",
+                        Location = new Location(2,2)
+                    },
+                    new Photo
+                    {
+                        Title="Third Image",
+                        Source="http://www.fillmurray.com/200/300",
+                        Location = new Location(3,3)
+                    },
+                    new Photo
+                    {
+                        Title="Fourth Image",
+                        Source="http://www.placebear.com/200/300",
+                        Location = new Location(4,4)
+                    }
+                });
+        }
+    }
+}
